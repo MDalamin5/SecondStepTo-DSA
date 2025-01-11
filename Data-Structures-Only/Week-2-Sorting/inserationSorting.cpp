5b08fedd19c4696f4a4df861d73f34c01784c845
MDalamin5 <mdal.amin5@northsouth.edu>
2025-01-11 10:39:06 +0600

    implement inseartion sort

--- Data-Structures-Only/Week-2-Sorting/inserationSorting.cpp
+++ Data-Structures-Only/Week-2-Sorting/inserationSorting.cpp
@@ -1,6 +1,35 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-    cout<<"test line";
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    
+    // Take input from User
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+
+
+    // Sorting Loop
+    for(int i=1; i<n; i++){
+        int idx=i;
+        while (idx>=1)
+        {
+            if(a[idx] < a[idx-1]){
+                swap(a[idx], a[idx-1]);
+                idx--;
+            }
+            else
+                break;
+        }
+        
+    }
+
+    // Output printing Loop
+    for(int i=0; i<n; i++)
+        cout<<a[i]<<" ";
+
+    
     return 0;
 }
