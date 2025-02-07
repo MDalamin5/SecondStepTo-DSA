8d88cc5d426f93faa8a9b28d378584f957c1902c
MDalamin5 <mdal.amin5@northsouth.edu>
2025-02-08 00:31:46 +0600

    implement get max and extract max elements

---
+++ Data-Structures-Only/Week-9-Heap/Module-32-max-heap/getMax-extractMax.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Heap {
+public:
+    vector<int> node;
+    Heap(){
+
+    }
+    void up_heapify(int idx){
+        while(idx > 0 && node[idx] > node[(idx - 1)/2]){
+            swap(node[idx], node[(idx - 1)/2]);
+            idx = (idx - 1) / 2;
+        }
+    }
+
+    void insert(int data){
+        node.push_back(data);
+        up_heapify(node.size() - 1);
+    }
+
+    void displayHeap(){
+        for(int i=0; i<node.size(); i++){
+            cout<<node[i]<<" ";
+        }
+        cout<<endl;
+    }
+
+    void DeleteElement(int idx){
+        if(idx > node.size())
+            return;
+            
+        swap(node[idx], node[node.size() - 1]);
+        node.pop_back();
+        downHeapiFi(idx);
+    }
+
+    void downHeapiFi(int idx){
+        
+            while(1){
+                int largest = idx;
+                int l = (2*idx) + 1;
+                int r = (2*idx) + 2;
+
+                if(l < node.size() && node[l] > node[largest]){
+                    largest = l;
+                }
+                if(r < node.size() && node[r] > node[largest]){
+                    largest = r;
+                }
+                if(largest == idx){
+                    break;
+                }
+                swap(node[idx], node[largest]);
+                idx = largest;
+            }
+        
+        
+    }
+
+    int getMax(){
+        if(!node.empty()){
+            return node[0];
+        }
+        else{
+            cout<<"Heap is empty"<<endl;
+            return -1;
+        }
+    }
+
+    int extractMax(){
+        int max = node[0];
+        DeleteElement(0);
+        return max;
+    }
+};
+
+int main(){
+    Heap hp;
+    hp.insert(1);
+    hp.insert(10);
+    hp.insert(5);
+    hp.insert(4);
+    hp.insert(11);
+    hp.insert(9);
+
+    hp.displayHeap();
+    cout<<hp.getMax()<<endl;
+    cout<<"After extracting max"<<endl;
+    cout<<hp.extractMax()<<endl;
+    hp.displayHeap();
+    
+
+}
