247d5fc3fb1cff5291ead20ad7b8b29cb8b52152
MDalamin5 <mdal.amin5@northsouth.edu>
2025-02-09 19:54:05 +0600

    implement pair in cpp

---
+++ Data-Structures-Only/Week-9-Heap/Module-34-cpp-STL/pair.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main(){
+    pair<string, int> person;
+    person.first = "Md Al Amin";
+    person.second = 25;
+
+    cout<<person.first<<" "<<person.second<<endl;
+
+    vector<pair<int, int>> ad_array;
+    
+    
+    
+}
