1729c300c76f3bac82570ef0998bb46cf0cff9c4
MDalamin5 <mdal.amin5@northsouth.edu>
2025-03-09 19:55:32 +0600

    implement bfs pseudo code

--- Design-and-Analysis-of-Algorithms/Week-02-BFS-and-DFS/Module-4-BFS-Searching/bfs-searching.cpp
+++ Design-and-Analysis-of-Algorithms/Week-02-BFS-and-DFS/Module-4-BFS-Searching/bfs-searching.cpp
@@ -1,1 +1,23 @@
 // Use queue to implement the bfs
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    cout<<"BFS Pseudo Code";
+}
+/*
+BFS Pseudo code
+BFS(src, graph):
+    init an empty queue and visited array
+    visited[src]=1 and q.push(src)
+    while(!q.empty()):
+        head=q.front()
+        q.pop()
+        output(head)
+        for adj_node of head:
+            if(visited[adj_node]==0):
+                visited[adj_node]=1 and q.push(adj_node)
+
+
+
+*/
