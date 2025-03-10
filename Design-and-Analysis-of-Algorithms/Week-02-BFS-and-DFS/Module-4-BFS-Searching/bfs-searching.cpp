7b4b4f509634db335aaa13def41a9fb816b65ae4
MDalamin5 <mdal.amin5@northsouth.edu>
2025-03-10 19:35:10 +0600

    bfs pseudo code and time complexity

--- Design-and-Analysis-of-Algorithms/Week-02-BFS-and-DFS/Module-4-BFS-Searching/bfs-searching.cpp
+++ Design-and-Analysis-of-Algorithms/Week-02-BFS-and-DFS/Module-4-BFS-Searching/bfs-searching.cpp
@@ -10,14 +10,15 @@ BFS Pseudo code
 BFS(src, graph):
     init an empty queue and visited array
     visited[src]=1 and q.push(src)
-    while(!q.empty()):
+    while(!q.empty()):  O(v)
         head=q.front()
         q.pop()
         output(head)
-        for adj_node of head:
+        for adj_node of head: O(2E)
             if(visited[adj_node]==0):
                 visited[adj_node]=1 and q.push(adj_node)
 
-
+total time complexity = O(v+E)
+space complexity = O(V) // v=vertex, e=edges
 
 */
