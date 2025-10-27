ff7e2cd6eae2842b6e2804f8ef9ae6035c476743
mdalamin5 <mdal.amin5@northsouth.edu>
2025-10-27 19:43:44 +0600

    graph representatin using adjacency list

---
+++ Design-and-Analysis-of-Algorithms/Week-1-Recape/graph_adjacency_list.cpp
@@ -0,0 +1,19 @@
+/*
+Graph Representations
+Topics: Adjacency list
+
+0-----1
+     / \
+    2---3
+
+* total node = 4
+* total edge = 4
+* assume its undirected and unweighted
+
+So the matrix will be -> num. of node x num. of node
+                      -> 4 x 4
+
+matrix[i][j] = 1
+if and only if (i->j) using only one edge
+
+*/
