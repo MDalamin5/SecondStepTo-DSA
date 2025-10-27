98a74b3ad977b09349d28449f469478bf1f05607
mdalamin5 <mdal.amin5@northsouth.edu>
2025-10-27 20:03:27 +0600

    adj list for graph

--- Design-and-Analysis-of-Algorithms/Week-1-Recape/graph_adjacency_list.cpp
+++ Design-and-Analysis-of-Algorithms/Week-1-Recape/graph_adjacency_list.cpp
@@ -16,4 +16,31 @@ So the matrix will be -> num. of node x num. of node
 matrix[i][j] = 1
 if and only if (i->j) using only one edge
 
+list:
+0 --> 1
+1 --> 0, 2, 3
+2 --> 3, 1
+3 --> 2, 1
+
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int nodes = 4;
+    vector<int> adj_list[nodes];
+
+    adj_list[0] = {1};
+    adj_list[1] = {0, 2, 3};
+    adj_list[2] = {3, 1};
+    adj_list[3] = {2, 1};
+
+    // Displaying the adj_list
+    for(int i=0; i<nodes; i++){
+        cout<<i<<"-->";
+        for(int j=0; j<adj_list[i].size(); j++){
+            cout<<adj_list[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
