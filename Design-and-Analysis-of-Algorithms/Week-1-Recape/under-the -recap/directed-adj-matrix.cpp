2242bd7adeb29332a8478d0f0ade44d2ceb08cb3
mdalamin5 <mdal.amin5@northsouth.edu>
2026-01-16 10:27:26 +0600

    graph representations done

--- Design-and-Analysis-of-Algorithms/Week-1-Recape/under-the -recap/directed-adj-matrix.cpp
+++ Design-and-Analysis-of-Algorithms/Week-1-Recape/under-the -recap/directed-adj-matrix.cpp
@@ -1,6 +1,39 @@
+/*
+0-----1
+     / \
+    2---3
+
+0 --> 1, 2
+1 --> 2, 4
+1 --> 3, 6
+2 --> 1, 1
+2 --> 3, 6
+3 --> 2, 4
+3 --> 1, 3
+
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    cout<<"Test line"<<endl;
+    int node = 4;
+    vector<pair<int, int>> adj_list[4] = {};
+
+    adj_list[0] = {{1, 2}};
+    adj_list[1] = {{2, 4}, {3, 6}};
+    adj_list[2] = {{1, 1}, {3, 6}};
+    adj_list[3] = {{2, 4}, {1, 3}};
+
+    
+    // printing adjacency list
+    for (int i = 0; i < node; i++) {
+        cout << i << " --> ";
+        for (auto edge : adj_list[i]) {
+            cout << "(" << edge.first << ", " << edge.second << ") ";
+        }
+        cout << endl;
+    }
+
 }
