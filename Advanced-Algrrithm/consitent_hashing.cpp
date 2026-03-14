0d01390f59d6b46f16a98eac5d44905ff5e8a12a
mdalamin5 <mdal.amin5@northsouth.edu>
2026-03-14 15:10:05 +0600

    hashmap sclatone

--- Advanced-Algrrithm/consitent_hashing.cpp
+++ Advanced-Algrrithm/consitent_hashing.cpp
@@ -1,1 +1,6 @@
-loading....consistent hashing
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"All About consistence hashing..."<<endl;
+    return 0;
+}
--- Advanced-Algrrithm/hashmap.cpp
+++ Advanced-Algrrithm/hashmap.cpp
@@ -1,1 +1,23 @@
-#include
+#include<bits/stdc++.h>
+using namespace std;
+
+class hashmap
+{
+private:
+    /* data */
+public:
+    hashmap(/* args */);
+    ~hashmap();
+};
+
+hashmap::hashmap(/* args */)
+{
+}
+
+hashmap::~hashmap()
+{
+}
+
+int mina(){
+    return 0;
+}
